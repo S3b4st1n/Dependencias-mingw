01fff9198acc0a570465830a8ba2413025f7e57c
S3b4st1n <sebastian.mercadomartinez@gmail.com>
2021-05-20 10:02:37 -0300

    Update deps-mingw.sh

--- deps-mingw.sh
+++ deps-mingw.sh
@@ -7,24 +7,24 @@ CFGPARMS_JANSON="--prefix=/jannson --host=mingw32"
 tar -xvf GTK3.0/openssl-1.0.0e.tar.gz 
 cp -rf openssl-1.0.0e /openssl
 cd /openssl 
-./Configure ${CFGPARMS_SSL} 2>&1 
-make -j4 2>&1 
+./Configure ${CFGPARMS_SSL} 
+make -j4  
 cp /openssl/libssl.pc /usr/src/mxe/usr/i686-w64-mingw32.shared/lib/pkgconfig/
 cp /openssl/libcrypto.pc /usr/src/mxe/usr/i686-w64-mingw32.shared/lib/pkgconfig/
 cp /openssl/openssl.pc /usr/src/mxe/usr/i686-w64-mingw32.shared/lib/pkgconfig/
 cd -
 tar -xvf GTK3.0/jansson-2.7.tar.gz      
 cd jansson-2.7       
-./configure ${CFGPARMS_JANSON} 2>&1 
-make -j4 2>&1                                            
-make -j4 install 2>&1
+./configure ${CFGPARMS_JANSON}  
+make -j4                                            
+make -j4 install 
 cp jansson.pc /usr/src/mxe/usr/i686-w64-mingw32.shared/lib/pkgconfig/
 cd ..
 tar -xvf GTK3.0/xerces-c-3.1.1.tar.gz 
 cd xerces-c-3.1.1          
-./configure ${CFGPARMS_XERCES} 2>&1
-make -j4 2>&1                                            
-make -j4 install 2>&1
+./configure ${CFGPARMS_XERCES} 
+make -j4                                           
+make -j4 install 
 cp xerces-c.pc /usr/src/mxe/usr/i686-w64-mingw32.shared/lib/pkgconfig/
 cd ..
 
